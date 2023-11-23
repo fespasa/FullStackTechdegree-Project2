e88078e080b5156f7d1d06f75f7a437709114646
fespasa <ferranespasa@gmail.com>
2023-11-23 11:05:14 +0100

    showPage function creation

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -3,7 +3,11 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+const header = document.querySelector('.header');
+const studentList = document.querySelector('.student-list');
+const linkList = document.querySelector('.link-list');
 
+const studentsPerPage = 9;
 
 /*
 For assistance:
@@ -18,7 +22,30 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+const showPage = (list, page) => {
+   const start = (page * studentsPerPage) - studentsPerPage;
+   const end = (page * studentsPerPage) - 1;
 
+   studentList.innerHTML = '';
+
+   for ( i = 0; i < list.length; i++) {
+      if ( i >= start && i <= end ) {
+         const html = `
+         <li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src=${list[i].picture.thumbnail} alt="Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined ${list[i].registered.date}</span>
+          </div>
+        </li>
+         `;
+         studentList.insertAdjacentHTML("beforeend", html);
+      }
+   }
+}
 
 /*
 Create the `addPagination` function
@@ -28,3 +55,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 1);
